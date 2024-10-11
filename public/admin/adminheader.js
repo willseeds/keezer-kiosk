36884fc8071ec6e6217a6c689d260a1cc477801b
Will S. <66182247+willseeds@users.noreply.github.com>
2024-10-11 15:13:26 -0700

    admin content

---
+++ public/admin/adminheader.js
@@ -0,0 +1,17 @@
+document.write(`
+
+    <div style="text-align:center;">
+      <table cellpadding="3" cellspacing="1"  style="box-sizing: border-box; vertical-align: bottom; position:relative; display: inline-table; width:90%; background:none; border: 1px solid; table-layout: fixed; ">
+        <tr>
+          <td width="90%" style="vertical-align: top; overflow:hidden; ">
+            <div style="text-align:center;">
+              <a href="/admin/">Admin Home</a>  |  <a href="/admin/add/">Add</a>  |  <a href="/admin/update/">Update</a>  |  <a href="/admin/delete/">Delete</a>
+            </div>
+          </td>
+        </tr>
+      </table>
+    </div>
+
+`);
+
+
