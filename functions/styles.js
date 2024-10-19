cf16cc654bf0061b08a4b5fda11069cec2703d35
Will S. <66182247+willseeds@users.noreply.github.com>
2024-10-19 10:44:09 -0700

    still at it

--- functions/styles.js
+++ functions/styles.js
@@ -1,14 +1,10 @@
-import { turso } from "@/lib/turso";
+import { createClient } from "@libsql/client";
 
-export default async function Page() {
-  const { rows } = await turso.execute("SELECT * FROM stylelist order by stylecolor ASC");
+const client = createClient({
+    url: process.env.TURSO_DATABASE_URL,
+    authToken: process.env.TURSO_AUTH_TOKEN,
+});
 
-  return (
-    <ul>
-      {rows.map((row) => (
-        <li key={row.id}>{row.id}</li>
-      ))}
-    </ul>
-  );
-}
+const result = await client.execute("SELECT * FROM stylelist order by stylecolor ASC");
 
+console.log("Styles:", result.rows);
